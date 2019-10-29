6e96489a0d88dac93c1d4cccdb5f761d35d8d470
Diego <48282657+Mijayo@users.noreply.github.com>
2019-10-29 11:11:00 +0100

    Comm2

--- js/tareas.js
+++ js/tareas.js
@@ -1,112 +1,10 @@
-////// EXAMINAR EL DOM //////
-/*console.dir(document);
-console.log('URL ' + document.URL);
-document.title = 'Hackeando el HTML';
-console.log(document.title);
-console.log(document.body);
-console.log(document.all);
-
-///// OBTENER DEL DOM LOS ID, CLASES, TAGS ... /////
-console.log(document.getElementById('main'));
-console.log(document.querySelectorAll('input'));
-var headerTitle = document.getElementById('header-title');
-headerTitle.textContent = 'Hackeando el HTML';
-
-var h2Title = document.getElementsByClassName('list-group-item');
-console.log(h2Title);
-
-for (var i = 0; i < h2Title.length; i++) {
-    h2Title[i].style.backgroundColor = "red";
-    h2Title[i].style.color = "white";
-    h2Title[i].innerHTML = 'Javo99';
-}*/
-
-//// PARENT NODE /////
-/*var itemsList = document.querySelector('#items');
-console.log(itemsList.parentNode);
-var main = itemsList.parentNode;
-main.style.backgroundColor = 'steelblue';
-console.log(main.parentNode.parentNode);
-var container = main.parentNode;
-container.style.backgroundColor = 'pink';
-container.style.padding = '30px';
-
-//// ELEMENT NODE /////
-var itemsList = document.querySelector('#items');
-console.log(itemsList.parentElement);
-var main = itemsList.parentElement;
-main.style.backgroundColor = 'purple';
-console.log(main.parentElement);
-var container = main.parentElement;
-container.style.backgroundColor = 'orange';
-container.style.padding = '30px';
-
-
-var itemsList = document.querySelector('#items');
-
-/// CHILDNODES ////
-console.log(itemsList.childNodes);
-/// CHILDREN ///
-console.log(itemsList.children);
-/// FIRSTCHILD / FIRSTELEMENTCHILD ///
-console.log(itemsList.firstElementChild);
-itemsList.firstElementChild.textContent = 'prueba';
-/// LASTCHILD / LASTELEMENTCHILD ///
-console.log(itemsList.lastElementChild);
-
-var itemsList = document.querySelector('#items');
-
-/// PREVIOUSSIBLING / PREVIOUSELEMENTSIBLING ///
-console.log(itemsList.previousSibling);
-console.log(itemsList.previousElementSibling);
-/// NEXTSIBLING / NEXTELEMENTSIBLING ///
-console.log(itemsList.nextSibling);
-console.log(itemsList.nextElementSibling);
-
-/// CREAR ELEMENTOS - tagName ///
-var nuevoDiv = document.createElement('div');
-nuevoDiv.className = 'hola';
-nuevoDiv.setAttribute('title', 'Hola mundo');
-nuevoDiv.innerHTML = '<h2>Hola Diego</h2>';
-console.log(nuevoDiv);
-
-/// CREAR TEXTNODE ///
-var nuevoNodoTXT = document.createTextNode('Hola mundo');
-nuevoDiv.appendChild(nuevoNodoTXT);
-
-var contenedor = document.querySelector('header .container');
-var h1 = document.querySelector('h1');
-
-contenedor.insertBefore(nuevoDiv, h1);*/
-
-
-///// AÑADIR EVENTOS A BOTONES DE DOS FORMAS //////
-
-// 1 //
-/*document.getElementById('boton').addEventListener('click', function() {
-    alert('click 2');
-});
-
-// 2 //
-document.getElementById('boton').addEventListener('click', botonPulsado);
-
-function botonPulsado() {
-    // alert('prueba');
-    var h = document.getElementById('header-title');
-    if (h.innerText === 'Lista de Items') {
-        h.innerText = 'Prueba';
-
-    } else {
-        h.innerText = 'Lista de Items';
-    }
-}*/
-
-
 //// CAMBIAR EL DISEÑO DEL SITE AÑADIENDO COSAS EN EL HTML /////
+// localStorage.clear();
 
 /// AGREGAR UN ELEMENTO A LA LISTA ///
 var form = document.getElementById('formAgregar');
 var lista = document.getElementById('items');
+var cont = 0;
 
 /// BUSCAR EL ELEMENTO ///
 var filtro = document.getElementById('filtro');
@@ -122,8 +20,28 @@ lista.addEventListener('click', eliminarItem);
 
 // Funcion para agregar
 function agregarItem(a) {
+
     a.preventDefault();
     var nuevoItem = document.getElementById('itemAgregar').value;
+    // console.log(nuevoItem);
+
+    // Agregarlo a un array
+    var arrayLocal = [];
+
+    // Creamos el objeto con los datos de las tareas con el ID en auto increment
+    var itemObject = {
+        'id': cont++,
+        'tarea': nuevoItem
+    };
+
+    arrayLocal.push(itemObject);
+    console.log(arrayLocal);
+
+    // Meterlo en localStorage
+    localStorage.setItem('nuevaTarea', JSON.stringify(itemObject));
+
+    var storedTareas = JSON.parse(localStorage.getItem('nuevaTarea'));
+
 
     var li = document.createElement('li');
     li.className = 'list-group-item';
@@ -147,6 +65,8 @@ function eliminarItem(e) {
     e.preventDefault();
     if (e.target.classList.contains('eliminar')) {
         if (confirm('¿Seguro que quiere eliminarlo?')) {
+            // Quitar del localStorage
+            localStorage.removeItem("nuevaTarea");
             var li = e.target.parentElement;
             lista.removeChild(li);
         }
