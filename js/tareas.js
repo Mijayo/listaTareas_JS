ae1bcc28aad7aa47cc8b884eeeb6e56c643601c1
Diego <48282657+Mijayo@users.noreply.github.com>
2019-10-30 18:00:27 +0100

    Comm6

--- js/tareas.js
+++ js/tareas.js
@@ -1,7 +1,7 @@
-//// CAMBIAR EL DISEÑO DEL SITE AÑADIENDO COSAS EN EL HTML /////
-// localStorage.clear();
+// Cada vez que se refresca la pagina se vacia el localStorage
+localStorage.clear();
 
-/// AGREGAR UN ELEMENTO A LA LISTA ///
+/// AGREGA UN ELEMENTO A LA LISTA ///
 var form = document.getElementById('formAgregar');
 var lista = document.getElementById('items');
 var cont = 0;
@@ -23,12 +23,11 @@ function agregarItem(a) {
 
     a.preventDefault();
     var nuevoItem = document.getElementById('itemAgregar').value;
-    // console.log(nuevoItem);
 
     // Creamos el objeto con los datos de las tareas con el ID en auto increment
     var itemObject = {
-        'id': `'${cont++}'`,
-        'tarea': `${nuevoItem}`
+        id: `${cont++}`,
+        tarea: `${nuevoItem}`
     };
 
     // Definimos la variable del array para almacenar el objeto itemObject
@@ -40,49 +39,77 @@ function agregarItem(a) {
     } else {
         arrayLocal = JSON.parse(localStorage.getItem('nuevaTarea'));
     }
+
     // 
     arrayLocal.push(itemObject);
-    // console.log(arrayLocal);
 
     // Meterlo en localStorage
     localStorage.setItem('nuevaTarea', JSON.stringify(arrayLocal));
 
-
-
+    // Pintamos los li en el HTML
     var li = document.createElement('li');
     li.className = 'list-group-item';
     li.appendChild(document.createTextNode(nuevoItem));
 
+    // Pintamos el boton X 
     var butDel = document.createElement('button');
-    butDel.className = 'btn btn-danger btn-sm float-right eliminar';
-    butDel.appendChild(document.createTextNode('x'));
+    butDel.className = `btn btn-danger btn-sm float-right eliminar `;
+    butDel.setAttribute('id', itemObject.id);
+
+    butDel.appendChild(document.createTextNode('X'));
 
     li.appendChild(butDel);
-
     lista.appendChild(li);
 
-    // limpia el campo de texto y lo deja vacio
-    document.getElementById('itemAgregar').value = ' ';
+    // Cuando se haga click en la X vamos a capturar el ID de la tarea que ha sido clicada
+    butDel.addEventListener('click', function(e) {
 
+        var idBoton;
+        // Obtenemos el id
+        idBoton = e.target.getAttribute('id');
+
+        // Controlamos que el ID no este vacio
+        if (idBoton !== null) {
+            // Llamamos a la funcion eliminarItem() pasandole el parametro del ID
+            eliminarItem(idBoton);
+        }
+
+    });
+
+    // Limpia el campo de texto y lo deja vacio
+    document.getElementById('itemAgregar').value = ' ';
 }
 
-// Funcion para eleminar de la lista 
-function eliminarItem(e) {
-    e.preventDefault();
-    if (e.target.classList.contains('eliminar')) {
-        if (confirm('¿Seguro que quiere eliminarlo?')) {
-            // Quitar del localStorage
-            localStorage.removeItem("nuevaTarea");
 
-            var li = e.target.parentElement;
-            lista.removeChild(li);
+// Funcion para eleminar de la lista // El parametro ID es el id recogido en la funcion click
+function eliminarItem(ID) {
+
+    var tareas = JSON.parse(localStorage.getItem("nuevaTarea"));
+
+    // ForEach para recorrer todas las tareas del localStorage
+    tareas.forEach((element, index) => {
+
+        // Controlamos que el ID del elemento sea igual al parametro recogido con el evento on click
+        if (element.id === ID) {
+            // Antes de eliminar la tarea preguntamos si quiere o no eliminarla
+            if (confirm('¿Desea eliminar la tarea?')) {
+                // Elimina del localStorage conforme el indice y la posicion
+                tareas.splice(index, 1);
+
+                // Seleccionamos al padre del boton X con parentElement, que es todo el <li>
+                var liT = document.getElementById(ID).parentElement;
+                // Elimina todo el campo de la lista
+                liT.remove();
+            }
         }
-    }
+    });
+
+    // Actualiza el localStorage
+    localStorage.setItem('nuevaTarea', JSON.stringify(tareas));
 }
 
 // Funcion para buscar en la lista
 function filtrarItems(e) {
-    // e.preventDefault();
     var texto = e.target.value.toLowerCase();
     var items = lista.getElementsByTagName('li');
     Array.from(items).forEach(function(item) {
@@ -93,5 +120,4 @@ function filtrarItems(e) {
             item.style.display = 'none';
         }
     });
-    //console.log(ite);
 }
